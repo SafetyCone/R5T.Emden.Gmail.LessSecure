d8de21d076251a44ffce65983fe0b278377b9db8
MinexAutomation <minex.automation@gmail.com>
2020-07-17 03:51:13 -0700

    Moved IServiceCollection extensions file, added AddX() and AddXAction() methods.

---
+++ source/R5T.Emden.Gmail.LessSecure/Code/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+using R5T.Argos;
+using R5T.Dacia;
+using R5T.Gimpolis;
+
+
+namespace R5T.Emden.Gmail.LessSecure
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="EmailSender"/> implementation of <see cref="IEmailSender"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddEmailSender(this IServiceCollection services,
+            IServiceAction<IOptions<GmailAuthentication>> gmailAuthenticationOptions)
+        {
+            services
+                .AddSingleton<IEmailSender, EmailSender>()
+                .Run(gmailAuthenticationOptions)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="EmailSender"/> implementation of <see cref="IEmailSender"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<IEmailSender> AddEmailSenderAction(this IServiceCollection services,
+            IServiceAction<IOptions<GmailAuthentication>> gmailAuthenticationOptions)
+        {
+            var serviceAction = ServiceAction.New<IEmailSender>(() => services.AddEmailSender(
+                gmailAuthenticationOptions));
+
+            return serviceAction;
+        }
+    }
+}
